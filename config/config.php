07668387d70daccd05bc2c0e0ac0e6e677965a4d
Charly2 <papapitufo10@gmail.com>
2018-05-28 01:18:43 -0500

    dsf

--- config/config.php
+++ config/config.php
@@ -26,35 +26,7 @@ $mes = array('',"Ene","Feb","Mar","Abri","May","Jun","Jul","Agos","Sep","Oct","N
 
 
 
-function guardar($data,$table,$conn)
-{
-	$keys = array_keys($data);
-	$query = "INSERT INTO $table (";
-	$keysmap = "";$i=0;$valuesmap ="";
-	foreach ($keys as $key) {
-		$i++;
-		$keysmap .= $key;
-		$valuesmap .= "'".$data[$key]."'";
-		if ( $i< sizeof($keys)) {
-			$keysmap .= ',';
-			$valuesmap .= ',';
-		}	
-	}
-	$query.=$keysmap;
-	$query.=') VALUES (';
-	$query.=$valuesmap;
-	$query.=')';
-	/*echo $query;
-	print_r($reporte);*/
 
-	$result = $conn->query($query);
-
-	if ($result) {
-		return $conn->insert_id;
-	}else{
-		return  0;
-	}
-}
 
 
 function getRandomCode(){
